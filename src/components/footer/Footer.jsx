61dad994ed02db33e8bb73c8243e619fdb07aa0b
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2024-05-31 14:34:22 -0300

    add instagram logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,22 @@
 import styles from '@/components/footer/footer.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
-    return (
-      <div className={styles.container}>
-        <div className={styles.img}>
-          <Image src='/kriptonite-logo.png' alt='' width={80} height={50}/>
-          <h2 className={styles.p}>Kryptonite Agency</h2>
-          <h3 className={styles.h3}>Sabemos como derrotar a Superman</h3>
-        </div>
-        <div className={styles.rrss}>
-          <Image src='/instagram.png' alt='Instagram' width={60} height={50}/>
-        </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.img}>
+        <Image src='/kriptonite-logo.png' alt='' width={80} height={50} />
+        <h2 className={styles.p}>Kryptonite Agency</h2>
+        <h3 className={styles.h3}>Sabemos como derrotar a Superman</h3>
       </div>
-    )
-  }
-  
-  export default Footer
+      <div className={styles.rrss}>
+        <Link to="https://www.instagram.com/kryptoniteagency">
+          <Image src='/instagram.jpg' alt='Instagram' width={60} height={50} className={styles.img} />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
