1309e72228141db8bdd566d015b353bbb58feef4
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2024-06-06 01:19:10 -0300

    fix texts

--- src/app/agencia/page.jsx
+++ src/app/agencia/page.jsx
@@ -10,19 +10,22 @@ const Agency = () => {
       </div>
       <div>
         <p className={styles.p}>
-          En nuestra agencia de publicidad nos enorgullece ofrecer soluciones creativas e innovadoras para promover y posicionar marcas en el mercado. 
+          En nuestra agencia de publicidad nos enorgullece ofrecer soluciones creativas e innovadoras para promover y posicionar marcas en el mercado.
           Nuestro equipo de expertos multidisciplinarios está comprometido en ofrecer resultados excepcionales a nuestros clientes al implementar estrategias de marketing efectivas y campañas publicitarias impactantes.
-
+          <br />
+          <br />
           Nos destacamos por nuestra capacidad para comprender las necesidades y objetivos únicos de cada cliente, y adaptar nuestras estrategias en consecuencia. Mediante una combinación de investigación exhaustiva, 
           análisis de mercado y conocimiento profundo de las últimas tendencias, creamos campañas que generan un impacto duradero y aumentan la visibilidad de las marcas.
-
-          Nuestra agencia se destaca por trabajar en estrecha colaboración con nuestros clientes, estableciendo una relación de confianza y comunicación abierta. 
-          Nos esforzamos por comprender su visión y convertirla en una realidad tangible, utilizando nuestro talento creativo y experiencia en la industria.
-
-          Contamos con un equipo de talentosos creativos, diseñadores gráficos, especialistas en marketing digital y estrategas de contenido, 
-          quienes se mantienen actualizados con las últimas tendencias y tecnologías en el ámbito de la publicidad. Esto nos permite ofrecer soluciones que se adaptan a las necesidades cambiantes del mercado y 
-          garantizar que nuestras campañas sean relevantes y efectivas.
-
+          <br />
+          <br />
+          Nuestra agencia se destaca por trabajar en estrecha colaboración con nuestros clientes, estableciendo una relación de confianza y comunicación abierta. Nos esforzamos por comprender su visión y 
+          convertirla en una realidad tangible, utilizando nuestro talento creativo y experiencia en la industria.
+          <br />
+          <br />
+          Contamos con un equipo de talentosos creativos, diseñadores gráficos, especialistas en marketing digital y estrategas de contenido, quienes se mantienen actualizados con las últimas tendencias y 
+          tecnologías en el ámbito de la publicidad. Esto nos permite ofrecer soluciones que se adaptan a las necesidades cambiantes del mercado y garantizar que nuestras campañas sean relevantes y efectivas.
+          <br />
+          <br />
           En resumen, nuestra agencia de publicidad se destaca por su enfoque centrado en el cliente, creatividad sobresaliente, conocimiento profundo del mercado y un equipo dedicado y apasionado. 
           Trabajamos incansablemente para cumplir y superar las expectativas de nuestros clientes, y nos enorgullece ser parte de su éxito.
         </p>
--- src/app/central/page.jsx
+++ src/app/central/page.jsx
@@ -10,21 +10,29 @@ const Central = () => {
       </div>
       <div>
         <p className={styles.p}>
-          En nuestra agencia de publicidad nos enorgullece ofrecer soluciones creativas e innovadoras para promover y posicionar marcas en el mercado.
-          Nuestro equipo de expertos multidisciplinarios está comprometido en ofrecer resultados excepcionales a nuestros clientes al implementar estrategias de marketing efectivas y campañas publicitarias impactantes.
-
-          Nos destacamos por nuestra capacidad para comprender las necesidades y objetivos únicos de cada cliente, y adaptar nuestras estrategias en consecuencia. Mediante una combinación de investigación exhaustiva,
-          análisis de mercado y conocimiento profundo de las últimas tendencias, creamos campañas que generan un impacto duradero y aumentan la visibilidad de las marcas.
-
-          Nuestra agencia se destaca por trabajar en estrecha colaboración con nuestros clientes, estableciendo una relación de confianza y comunicación abierta.
-          Nos esforzamos por comprender su visión y convertirla en una realidad tangible, utilizando nuestro talento creativo y experiencia en la industria.
-
-          Contamos con un equipo de talentosos creativos, diseñadores gráficos, especialistas en marketing digital y estrategas de contenido,
-          quienes se mantienen actualizados con las últimas tendencias y tecnologías en el ámbito de la publicidad. Esto nos permite ofrecer soluciones que se adaptan a las necesidades cambiantes del mercado y
-          garantizar que nuestras campañas sean relevantes y efectivas.
-
-          En resumen, nuestra agencia de publicidad se destaca por su enfoque centrado en el cliente, creatividad sobresaliente, conocimiento profundo del mercado y un equipo dedicado y apasionado.
-          Trabajamos incansablemente para cumplir y superar las expectativas de nuestros clientes, y nos enorgullece ser parte de su éxito.
+          En nuestra central de medios nos dedicamos a ofrecer soluciones integrales y efectivas para la planificación, compra y ejecución de estrategias de medios.
+          Nuestro objetivo es ayudar a las marcas a maximizar su alcance e impacto en el mercado, optimizando cada dólar invertido en publicidad.
+          <br />
+          <br />
+          Contamos con un equipo de expertos en investigación de mercado, análisis de audiencia y seguimiento de tendencias, quienes se encargan de identificar 
+          las mejores oportunidades de medios para cada cliente. Además, tenemos relaciones sólidas con los principales medios de comunicación y plataformas digitales, 
+          lo que nos permite negociar las mejores tarifas y condiciones para nuestros clientes.
+          <br />
+          <br />
+          Nos enorgullece destacarnos por nuestra capacidad para adaptarnos a los cambios constantes en la industria de la publicidad, aprovechando las últimas tecnologías 
+          y herramientas de análisis de datos. Esto nos permite ofrecer estrategias de medios altamente personalizadas, asegurando que cada campaña sea lo más efectiva posible.
+          <br />
+          <br />
+          Además, nuestro enfoque se basa en una comunicación transparente y cercana con nuestros clientes. Trabajamos en estrecha colaboración con ellos para entender 
+          sus necesidades y objetivos, y brindarles un servicio personalizado y proactivo.
+          <br />
+          <br />
+          Si buscas una central de medios comprometida con el éxito de tu marca, confía en nosotros. Estamos aquí para ayudarte a alcanzar tus metas de visibilidad y 
+          crecimiento a través de estrategias de medios estratégicas y eficientes.
+          <br />
+          <br />
+          Compramos, vendemos y comercializamos medios tradicionales, medios digitales y vía pública así como también transporte público y nuevas tecnologías como 
+          puntos wifi y wifi en colectivos y trenes.
         </p>
       </div>
     </div>
